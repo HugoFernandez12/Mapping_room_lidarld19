7f5d1f7394f77588b8181e880d7fc545078faadf
Hugo Fernández Aliseda <hugofernandezaliseda@gmail.com>
2025-12-04 21:43:15 +0100

    Add files via upload

--- launch/ld19.launch.py
+++ launch/ld19.launch.py
@@ -1,1 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='lidar_ld19',
+            executable='ld19_node',
+            name='ld19_lidar_node',
+            output='screen',
+            parameters=[{
+                'port': '/dev/ttyUSB0',
+                'baudrate': 230400,
+                'frame_id': 'laser_frame',
+                'scan_frequency': 10.0
+            }]
+        )
+    ])
